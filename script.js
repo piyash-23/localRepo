63a9b0d62eed51a4c012246264726638ee20244c
piyash-23 <arafat.ahmed2004@gmail.com>
2025-05-15 22:14:24 +0600

    js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-let x ="pata";
-console.log(x);
+const profile = {
+    pName: "Shradha Khapra",
+    posts: 195,
+    followers: "894k",
+    following: 4,
+};
+console.log(profile)
+console.log(typeof profile);
