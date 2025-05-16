a685c8922dc40856625e23a0fc5f69a7545e5352
piyash-23 <arafat.ahmed2004@gmail.com>
2025-05-16 11:47:20 +0600

    added

--- script.js
+++ script.js
@@ -17,3 +17,16 @@ console.log("a-b =", a - b);
 console.log("a*b =", a * b);
 console.log("a/b =", a / b);
 console.log("The modulus of","a&b =", a % b);
+// for power we use **
+console.log("a to the power of b will be", a**b);
+
+// assignment operators 
+{
+    let a = 10;
+    console.log(a);
+    console.log(a += 4);
+    console.log(a -= 4);
+    console.log(a %= 4);
+    console.log(a /= 4);
+    console.log(a *= 4);
+}
