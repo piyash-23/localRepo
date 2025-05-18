f064c765a07f7d4fad649b4d45e113c1f58a1ba8
piyash-23 <arafat.ahmed2004@gmail.com>
2025-05-18 11:55:28 +0600

    add

--- script.js
+++ script.js
@@ -134,9 +134,23 @@ for(let i=1; i <=20; i+=2){
     */
 //  ================================================while loop=================================
 // while loop er jonno shurute condition define korte hobe erpore {} er vitor work likhte hobe
-
+/*
 let i = 1;
 while( i<=10 ){
     i++ ;
     console.log("i =", i);
 } // i er value browser 11 show korbe karon i 10 er por 11 run kore erpore statement check kore dkhe thamte hobe.
+*/
+// =================================== do while loop =============================
+
+let i=20;
+do{
+    i++;
+    console.log("i=", i);
+}
+while(i<= 10);
+// ekhane show korche karon shurute ekta output paiche erpore condition check korche
+while( i<= 10){
+    i++;
+    console.log("i=", i); // ekhane kichu show kore nai karon agei condition dekhe niche
+}
